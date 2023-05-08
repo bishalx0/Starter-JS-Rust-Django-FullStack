d023805ac8e33057ec07e6bb3cb030f0d7697e15
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-05-08 13:41:48 +0545

    add backend files

---
+++ back/src/main.rs
@@ -0,0 +1,28 @@
+use axum::{http::Method, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+
+use tower_http::cors::{Any, CorsLayer};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Dummy {
+    id: String,
+    msg: String,
+}
+
+async fn hello_world() -> Json<Dummy> {
+    Json(Dummy {
+        id: "1".to_string(),
+        msg: "Hello World".to_string(),
+    })
+}
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET])
+        .allow_origin(Any);
+    let app = Router::new().route("/hello", get(hello_world)).layer(cors);
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
